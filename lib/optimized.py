67a5df4b212f204b0b7f091d6dda52b4d8c3b257
Fra Par <francois.parenti@gmail.com>
2021-09-20 10:48:51 +0200

    Finished Algorythm

--- lib/optimized.py
+++ lib/optimized.py
@@ -5,11 +5,12 @@ import operator
 
 from model import All_Data
 
-csvFilePath = '../Stocks_data.csv'
+csvFilePath = '../dataset2_Python.csv'
+# csvFilePath = '../dataset1_Python.csv'
+# csvFilePath = '../Stocks_data.csv'
 jsonFilePath = '../json_file_name.json'
 list_of_tuples = []
 stocks_list = []
-data = {}
 
 # We are importing CSV file directly from our root repository
 # And creating the Json file to store data
@@ -19,7 +20,7 @@ def openFile(csvFilePath, jsonFilePath):
         next(csv_reader)
         index = 0
         for lines in csv_reader:
-            totalCalculate = round((float(lines[1])*float(lines[2])),2)
+            totalCalculate = round((float(lines[1])*float(lines[2])/100),2)
             dataTuple[index] = {"name":lines[0], "cost":float(lines[1]), "profit":float(lines[2]), "total_profit":totalCalculate}
             list_of_tuples.append(dataTuple[index])
             index += 1
@@ -30,35 +31,18 @@ def openFile(csvFilePath, jsonFilePath):
     with open(jsonFilePath, 'w') as jsonFile:
         jsonFile.write(json.dumps(list_of_tuples, indent=4))
 
+# Calculating the different possibilities for investing
 def findStocks(amount_in_bank):
     for actions in new_list:
         cost = actions["cost"]
-        new_amount_in_bank = amount_in_bank - int(cost)
-        if new_amount_in_bank >= 0:
-            amount_in_bank -= int(cost)
+        new_amount_in_bank = amount_in_bank - float(cost)
+        if new_amount_in_bank >= 0 and cost > 0:
+            amount_in_bank -= float(cost)
             stocks_list.append(actions["name"])
-        else:
-            next
-    with open(jsonFilePath, 'w') as jsonFile:
-        jsonFile.write(json.dumps(stocks_list, indent=4))
-    return stocks_list
+            with open(jsonFilePath, 'w') as jsonFile:
+                jsonFile.write(json.dumps(stocks_list, indent=4))
 
-# Calculate the profit for all valid stocks combination
-def calculateProfit(stocks_list):
-    total_profit = 0
-    for data in stocks_list:
-        print("data")
-        print(data)
-        print("distros_dict")
-        print(distros_dict)
-        print("distros_dict[0]")
-        print(distros_dict[0])
-
-        total_profit += distros_dict['total_profit']
-        if total_profit > most_profit:
-            most_profit = total_profit
-            combination_profit_most_value = data
-
+# Getting the value from the dictionnary
 def getKeysByValue(dictOfElements, valueToFind, totalCost, totalProfit):
     listOfItems = dictOfElements
     for item in listOfItems:
@@ -67,10 +51,16 @@ def getKeysByValue(dictOfElements, valueToFind, totalCost, totalProfit):
             totalProfit.append(float(item["total_profit"]))
     return totalCost, totalProfit
 
+# Print the stock solution for the user
+def printSolution():
+    print("The best Stocks to buy are :")
+    for data in stocks_list:
+        print(data)
+    print("For " + str(round(calculate_the_cost, 2)) + " it's worth " + str(round(calculate_the_profit, 2)) + " after 2 years!")
+
 dataTuple = {}
 most_profit = 0
 combination_for_most_value = []
-all_profit_data = calculateProfit(stocks_list)
 totalCost = []
 totalProfit = []
 
@@ -79,6 +69,7 @@ openFile(csvFilePath, jsonFilePath)
 with open(jsonFilePath, 'r') as file:
     distros_dict = json.load(file)
 
+# We sort the list by profit from the dataset
 new_list = sorted(list_of_tuples, key=operator.itemgetter("profit"), reverse = True)
 
 amount_in_bank = 500
@@ -88,13 +79,12 @@ findStocks(amount_in_bank)
 for data in stocks_list:
     listOfKeys = getKeysByValue(distros_dict, data, totalCost, totalProfit)
 
-calculate_the_Cost = 0
+calculate_the_cost = 0
 for cost in totalCost:
-    calculate_the_Cost += cost
+    calculate_the_cost += cost
 
-calculate_the_Profit = 0
+calculate_the_profit = 0
 for profit in totalProfit:
-    calculate_the_Profit += profit
+    calculate_the_profit += profit
 
-print(calculate_the_Cost)
-print(calculate_the_Profit)
+printSolution()
